9498e377fd1f75bfa9f3f63ef9a82a1653a9e7b1
Sol Tolosa <solangel.zhuo@gmail.com>
2026-04-27 21:48:42 +0100

    test

--- python.py
+++ python.py
@@ -18,8 +18,7 @@ directorio_destino = 'extraccion/'
 # Descomprimir el archivo JAR
 unzip_jar(ruta_jar, directorio_destino)
 
-# Extraer el archivo mifichero.properties
-archivo_a_extraer = 'mifichero.properties'
+
 extract_file_from_jar(ruta_jar, archivo_a_extraer, directorio_destino)
 
 print(f"El archivo '{archivo_a_extraer}' ha sido extraído exitosamente en '{directorio_destino}'.")
